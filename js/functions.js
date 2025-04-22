ea9e5870da1d37e1aeef1c4a40710b66e21219c8
itsAsh7 <166305668+itsAsh7@users.noreply.github.com>
2025-04-22 17:40:41 +0530

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -92,28 +92,27 @@ function startHeartAnimation() {
 	};
 })(jQuery);
 
-function timeElapse("Apr 22, 2024 15:37:25"){
-	var current = Date("Apr 22, 2025 15:37:25");
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
-	$("#elapseClock").html(result);
-}
+function timeElapse() {
+    var startDate = new Date("Apr 22, 2024 15:37:25");
+    var current = new Date(); // current time
+    var seconds = Math.floor((current - startDate) / 1000); // total elapsed seconds
 
+    var days = Math.floor(seconds / (3600 * 24));
+    seconds = seconds % (3600 * 24);
+
+    var hours = Math.floor(seconds / 3600);
+    if (hours < 10) hours = "0" + hours;
+    seconds = seconds % 3600;
+
+    var minutes = Math.floor(seconds / 60);
+    if (minutes < 10) minutes = "0" + minutes;
+    seconds = seconds % 60;
+
+    if (seconds < 10) seconds = "0" + seconds;
+
+    var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
+    $("#elapseClock").html(result);
+}
 function showMessages() {
 	adjustWordsPosition();
 	$('#messages').fadeIn(5000, function() {
