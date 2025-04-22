6a0f05f344a0c046e1739e51a46fd5b5c27f357e
itsAsh7 <166305668+itsAsh7@users.noreply.github.com>
2025-04-22 17:17:05 +0530

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -93,7 +93,7 @@ function startHeartAnimation() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
+	var current = Date("Apr 22, 2024 15:37:25");
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
